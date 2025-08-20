59229d4b4b4385f31bd42b08324a230f36eb6b19
sik2 <bickwang123@gmail.com>
2025-08-20 12:39:27 +0900

    autowired

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,16 +1,23 @@
 package com.back.global.initData;
 
 
+import com.back.domain.post.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BaseInitData {
+
+    @Autowired
+    private PostRepository postRepository;
+
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-
+            postRepository.count();
+            // SELECT * FROM post;
         };
     }
 }
