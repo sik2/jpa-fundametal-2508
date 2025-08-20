155eefa0d84fbb5a3af57a217b02884c23dbc293
sik2 <bickwang123@gmail.com>
2025-08-20 11:47:43 +0900

    post entity

---
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -0,0 +1,16 @@
+package com.back.domain.post.post.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity // 아래 구조대로 DB 테이블(컬럼)을 만들어야 한다.
+public class Post {
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private int id; // INT
+    private String title; // VARCHAR(255)
+}
+
