4ad46efc95a9b5fbfde04faab1bec202ffe40cd4
sik2 <bickwang123@gmail.com>
2025-08-21 11:31:02 +0900

    self

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -4,9 +4,11 @@ package com.back.global.initData;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -14,13 +16,17 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Configuration
 public class BaseInitData {
+    @Autowired
+    @Lazy
+    private BaseInitData self;
+
     private final PostService postService;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
 
