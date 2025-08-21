bdf82a58f5ee91e0bebafa9d968ac67580bac8cf
sik2 <bickwang123@gmail.com>
2025-08-21 10:12:24 +0900

    rollback final

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -2,11 +2,11 @@ package com.back.domain.post.post.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity // 아래 구조대로 DB 테이블(컬럼)을 만들어야 한다.
@@ -14,20 +14,14 @@ import lombok.ToString;
 public class Post {
     @Id // PK
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
-    private final int id; // INT
-    private final String title; // VARCHAR(255)
+    private int id; // INT
+    private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
-    private final String content; // TEXT
+    private String content; // TEXT
 
-
-    public Post (String title, String content) {
-        this.id = 0;
-        this.title = title;
-        this.content = content;
-    }
-
-    public Post() {
-        this("", "");
+    public Post(String title, String content) {
+            this.title = title;
+            this.content = content;
     }
 }
 
