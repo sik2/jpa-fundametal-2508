ac779e97d691e90a31c4e726de8ec080e4fb366a
sik2 <bickwang123@gmail.com>
2025-08-21 10:33:21 +0900

    service

--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.back.domain.post.post.repository;
 
 import com.back.domain.post.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 }
--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -3,11 +3,11 @@ package com.back.domain.post.post.service;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Component
+@Service
 public class PostService {
 
     @Autowired
