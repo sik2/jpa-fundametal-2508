79e49d4bbf9381479de2c972b0dcc22e40547b65
sik2 <bickwang123@gmail.com>
2025-08-21 10:40:54 +0900

    AllArgsConstructor

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -3,19 +3,17 @@ package com.back.global.initData;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Optional;
 
+@AllArgsConstructor
 @Configuration
 public class BaseInitData {
     private PostService postService;
-
-    public BaseInitData (PostService postService) {
-        this.postService = postService;
-    }
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
