4d97e91a491ccf1599cc7aadeaee173167e51936
sik2 <bickwang123@gmail.com>
2025-08-22 12:34:28 +0900

    member

---
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -0,0 +1,21 @@
+package com.back.domain.member.member.entity;
+
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Member extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String nickname;
+}
