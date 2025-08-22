105bc3cbd920583df079dc0f38b2167a8235f54d
sik2 <bickwang123@gmail.com>
2025-08-22 11:40:28 +0900

    del rollback

--- src/test/java/com/back/domain/post/post/repository/PostRepositoryTest.java
+++ src/test/java/com/back/domain/post/post/repository/PostRepositoryTest.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Transactional
-@Rollback
 @SpringBootTest
 public class PostRepositoryTest {
     @Autowired
