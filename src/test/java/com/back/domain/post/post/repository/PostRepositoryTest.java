d89e86543c9c9f02ee597f8b317e82e8bec50205
sik2 <bickwang123@gmail.com>
2025-08-22 11:20:23 +0900

    test write

--- src/test/java/com/back/domain/post/post/repository/PostRepositoryTest.java
+++ src/test/java/com/back/domain/post/post/repository/PostRepositoryTest.java
@@ -15,10 +15,20 @@ public class PostRepositoryTest {
 
     @Test
     @DisplayName("2번 글 조회")
-    void t1 () {
+    void t1() {
         Post post2 = postRepository.findById(2).get();
 
         assertThat(post2.getTitle()).isEqualTo("제목 2");
         assertThat(post2.getContent()).isEqualTo("내용 2");
+    }
+
+    @Test
+    @DisplayName("글 생성")
+    void v2() {
+        Post post = new Post("새 제목", "새 내용");
+        Post createdPost = postRepository.save(post);
+
+        assertThat(createdPost.getTitle()).isEqualTo("새 제목");
+        assertThat(createdPost.getContent()).isEqualTo("새 내용");
     }
 }
